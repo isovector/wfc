d17093d983494056d72eac4137c8c8eda7c9b85e
Sandy Maguire <sandy@sandymaguire.me>
2018-11-28 14:24:47 -0500

    hey it performs pretty well all of a sudden

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE DataKinds                  #-}
 {-# LANGUAGE DeriveAnyClass             #-}
+{-# LANGUAGE DeriveFoldable             #-}
 {-# LANGUAGE DeriveFunctor              #-}
 {-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DeriveTraversable          #-}
 {-# LANGUAGE DerivingStrategies         #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE KindSignatures             #-}
@@ -36,11 +38,23 @@ import           Linear.V2
 deriving instance Generic PixelRGB8
 deriving instance NFData PixelRGB8
 
+data SuperPos a
+  = SuperPos (S.Set a)
+  | Collapsed a
+
+superposit :: (S.Set a -> S.Set a) -> SuperPos a -> SuperPos a
+superposit _ z@(Collapsed _) = z
+superposit f (SuperPos s)    = SuperPos $ f s
+
+superpose :: Ord b => (a -> b) -> SuperPos a -> SuperPos b
+superpose f (Collapsed z) = Collapsed $ f z
+superpose f (SuperPos s)  = SuperPos $ S.map f s
+
 
 newtype Grid (width :: Nat) (height :: Nat) a = Grid
     { getGrid :: Compose V.Vector V.Vector a
     }
-  deriving (Functor, Show, Foldable, Eq)
+  deriving (Functor, Show, Foldable, Eq, Traversable)
 
 instance (KnownNat m, KnownNat n) => Distributive (Grid m n) where
   distribute = distributeRep
@@ -117,6 +131,7 @@ wrap :: Int  -- ^ v
      -> (Int, Int)
 wrap x n
  | x < 0 = (0, x + n)
+ -- | x > n = wrap (x - n) n
  | otherwise = (x, n)
 
 
@@ -134,16 +149,6 @@ agrees n (V2 dx dy) (Pattern p1) (Pattern p2) = and $ do
       == p2 V.! packPosition n (V2 (x - dx) (y - dy))
 
 
-colors
-   :: (Pixel a, Ord a, NFData a)
-   => Image a
-   -> SuperPos a
-colors img = force $ S.fromList $ do
-  x <- [0..imageWidth img - 1]
-  y <- [0..imageHeight img - 1]
-  pure $ pixelAt img x y
-
-
 allPatterns
     :: (Pixel a, NFData a)
     => Int   -- ^ n
@@ -152,8 +157,8 @@ allPatterns
 allPatterns n img = force $ do
   y <- [0..imageHeight img]
   x <- [0..imageWidth img]
-  guard $ x + n < imageWidth img
-  guard $ y + n < imageHeight img
+  guard $ x + n <= imageWidth img
+  guard $ y + n <= imageHeight img
   let xy = V2 x y
   coerce $ fmap (\c -> packPattern n (c !* xy) img)
     -- TODO(sandy): this transforms should be in local space, not img space
@@ -171,14 +176,12 @@ type WIDTH = 32
 type HEIGHT = 32
 type Quantum = Store (Grid WIDTH HEIGHT)
 
-type SuperPos = S.Set
-
 initialize
     :: (Pixel a, Ord a, NFData a)
     => Int
     -> Image a
     -> Quantum (SuperPos (Int, Pattern a))
-initialize n img = store (const . S.fromList . frequency $ allPatterns n img) (V2 0 0)
+initialize n img = store (const . SuperPos . S.fromList . frequency $ allPatterns n img) (V2 0 0)
 
 
 inNSquare :: Int -> Coord -> Bool
@@ -190,18 +193,18 @@ inNSquare n (V2 x y) = and
   ]
 
 observe
-    :: Ord a
+    :: (Ord a, Show a)
     => Int  -- ^ n
-    -> Int  -- ^ seed
+    -> Pattern a
     -> Coord
     -> Quantum (SuperPos (Int, Pattern a))
     -> SuperPos (Int, Pattern a)
-observe n a xy w =
-  stamp n xy (weighted a xy $ extract $ seek xy w) w
+observe n p xy w =
+  stamp n xy p w
 
 
 stamp
-    :: Eq a
+    :: (Show a, Eq a)
     => Int
     -> Coord
     -> Pattern a
@@ -209,26 +212,36 @@ stamp
     -> SuperPos (Int, Pattern a)
 stamp n xy p w =
   let dxy@(V2 dx dy) = pos w - xy
-   in case abs dx < n && abs dy < n of
-        True -> S.filter (\(snd -> p') -> agrees n dxy p p')
-                  $ extract w
-        False -> extract w
+      q = extract w
+   in case q of
+        SuperPos ps ->
+          case (xy == pos w, -n + 1 <= dx && dx < n  && -n + 1 <= dy && dy < n) of
+            (True, _) -> Collapsed (0, p)
+            (_, True) -> SuperPos
+                  . S.filter (\(snd -> p') -> agrees n dxy p' p)
+                  $ ps
+            (_, False) -> q
+        Collapsed z -> Collapsed z
 
 
 entropy :: Quantum (SuperPos a) -> Int
-entropy = S.size . extract
+entropy w =
+  case extract w of
+    SuperPos z  -> S.size z
+    Collapsed _ -> 0
+
+
+getStore :: Store w a -> w a
+getStore (StoreT w _) = runIdentity w
 
 
 minEntropy :: Quantum (SuperPos a) -> (Coord, Int)
-minEntropy q = minimumBy (comparing fst) $ do
-    let f = fst . runStore $ q =>> pos &&& entropy
-    x <- [0..w-1]
-    y <- [0..h-1]
-    let xy = V2 x y
-    guard $ snd (f xy) > 1
-    pure $ f xy
-  where
-    (w, h) = getSize q
+minEntropy q
+  = minimumBy (comparing snd)
+  . filter ((> 0) . snd)
+  . toList
+  . getStore
+  $ q =>> pos &&& entropy
 
 
 patternToPixel :: Pattern a -> a
@@ -236,15 +249,21 @@ patternToPixel (Pattern v) = V.head v
 
 
 blend :: SuperPos PixelRGB8 -> PixelRGB8
-blend ps
+blend (Collapsed ps) = ps
+blend (SuperPos ps)
   | S.null ps = PixelRGB8 255 0 255
+  -- | S.size ps == 1 =
+  --     let ZipList [r, g, b] = unpack $ head $ toList ps
+  --      in (PixelRGB8 255 255 0)
   | otherwise =
-      let unpack = \(PixelRGB8 r g b) -> ZipList [r, g, b]
-          elems = fmap unpack $ toList ps
+      let elems = fmap unpack $ toList ps
           len = length elems
           ZipList [r', g', b'] =
             fmap (fromIntegral . (`div` len) . sum . fmap fromIntegral) $ sequenceA elems
       in PixelRGB8 r' g' b'
+  where
+    unpack (PixelRGB8 r g b) = ZipList [r, g, b]
+
 
 getSize :: forall m n a. (KnownNat m, KnownNat n) => Store (Grid m n) a -> (Int, Int)
 getSize _ = ( fromIntegral . natVal $ Proxy @m
@@ -255,7 +274,7 @@ getSize _ = ( fromIntegral . natVal $ Proxy @m
 render :: Quantum (SuperPos PixelRGB8) -> Image PixelRGB8
 render q = uncurry (generateImage makePixel) $ getSize q
   where
-    makePixel x y = peek (V2 x y) $ extend (blend . extract) q
+    makePixel x y = peek (V2 x y) $ fmap blend q
 
 
 twist :: Int -> Coord -> Integer
@@ -263,8 +282,9 @@ twist a (V2 x y) = (fromIntegral x ^ (5 :: Int) + fromIntegral y ^ (3 :: Int))
   `div` fromIntegral a
 
 
-weighted :: Int -> Coord -> SuperPos (Int, a) -> a
-weighted a xy (S.toList -> s) =
+weighted :: Show a => Int -> Coord -> SuperPos (Int, a) -> a
+weighted _ _ (Collapsed (_, s)) = s
+weighted a xy (SuperPos (S.toList -> s)) =
   let size = sum $ fmap fst s
       total = join $ fmap (uncurry replicate) s
    in total !! (fromIntegral (twist a xy `mod` fromIntegral size))
@@ -276,15 +296,15 @@ main = do
       q'' = iterate
               (\(a, q) ->
                 (a+1,) $
-                  q =>>
-                    (observe n (showTrace a)
-                      $ fst
-                    $ showTrace
-                      $ minEntropy q))
-          $ (2, initialize n bricks)
-      q' = snd $ q'' !! 400
-  writePng "result.png"
-    . render
-    $ extend (S.map patternToPixel . S.map snd . extract) q'
+                  let epos = showTrace $ fst $ minEntropy q
+                      collapsed = showTrace $ weighted a epos $ peek epos q
+                   in q =>> observe n collapsed epos)
+          $ (1, initialize n bricks)
+  for_ [0..200] $ \i -> do
+    let q' = snd $ q'' !! i
+    writePng "result.png"
+      . render
+      $ fmap (superpose (V.head . getPattern) . superpose snd) q'
+    threadDelay 250000
   pure ()
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,43 @@
+module Main where
+
+import           Control.Monad (guard, when)
+import           Data.Bool (bool)
+import           Data.Foldable (for_)
+import           Data.Tuple (swap)
+import qualified Data.Vector as V
+import           Lib hiding (main)
+import           Linear.V2
+import           Test.Hspec
+
+
+makePattern :: [a] -> Pattern a
+makePattern = Pattern . V.fromList
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  let boring  = makePattern $ replicate 9 0
+      middle  = makePattern [0, 0, 0, 0, 1, 0, 0, 0, 0]
+      checker = makePattern [0, 1, 0, 1, 0, 1, 0, 1, 0]
+
+  describe "your mum" $ do
+    it "should always be boring" $ do
+      for_ [-2..2] $ \y ->
+        for_ [-2..2] $ \x ->
+          agrees 3 (V2 x y) boring boring `shouldBe` True
+
+    it "should border" $ do
+      for_ [-2..2] $ \y ->
+        for_ [-2..2] $ \x -> do
+          agrees 3 (V2 x y) boring middle `shouldBe` (abs y == 2 || abs x == 2)
+          agrees 3 (V2 x y) middle boring `shouldBe` (abs y == 2 || abs x == 2)
+
+    it "should checker" $ do
+      for_ [-2..2] $ \y ->
+        for_ [-2..2] $ \x ->
+          agrees 3 (V2 x y) checker checker
+            `shouldBe` bool False True ((x + y) `mod` 2 == 0)
+
+  describe "tests" $ do
+    it "should work" $ do
+      frequency "hello" `shouldBe` [(1, 'e'), (1, 'h'), (2, 'l'), (1, 'o')]
+
